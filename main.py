9a3e2f1b268a7e0100a8f618747748b7753de8eb
Kali Live user <jakob_luge@mailbox.org>
2022-01-11 13:15:58 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,69 @@
+
+
+import paho.mqtt.client as mqtt
+from influxdb import InfluxDBClient
+
+INFLUXDB_ADDRESS = 'localhost'
+INFLUXDB_USER = 'admin'
+INFLUXDB_PASSWORD = 'admin'
+INFLUXDB_DATABASE = 'dht22'
+
+MQTT_ADDRESS = '172.30.131.70'
+MQTT_USER = 'cdavid'
+MQTT_PASSWORD = 'cdavid'
+MQTT_TOPIC = 'house/dht22'
+MQTT_CLIENT_ID = 'MQTTInfluxDBBridge'
+
+influxdb_client = InfluxDBClient(INFLUXDB_ADDRESS, 8086, INFLUXDB_USER, INFLUXDB_PASSWORD, None)
+
+
+
+def on_connect(client, userdata, flags, rc):
+    """ The callback for when the client receives a CONNACK response from the server."""
+    print('Connected with result code ' + str(rc))
+    client.subscribe(MQTT_TOPIC)
+
+
+
+def _send_sensor_data_to_influxdb(sensor_data):
+    json_body = [
+        {
+            'measurement': sensor_data.measurement,
+            'tags': {
+                'location': sensor_data.location
+            },
+            'fields': {
+                'value': sensor_data.value
+            }
+        }
+    ]
+    influxdb_client.write_points(json_body)
+
+def on_message(client, userdata, msg):
+    """The callback for when a PUBLISH message is received from the server."""
+    print(msg.topic + ' ' + str(msg.payload))
+    #sensor_data = _parse_mqtt_message(msg.topic, msg.payload.decode('utf-8'))
+    #if sensor_data is not None:
+        #_send_sensor_data_to_influxdb(sensor_data)
+
+def _init_influxdb_database():
+    databases = influxdb_client.get_list_database()
+    if len(list(filter(lambda x: x['name'] == INFLUXDB_DATABASE, databases))) == 0:
+        influxdb_client.create_database(INFLUXDB_DATABASE)
+    influxdb_client.switch_database(INFLUXDB_DATABASE)
+
+def main():
+    _init_influxdb_database()
+
+    mqtt_client = mqtt.Client(MQTT_CLIENT_ID)
+    mqtt_client.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(MQTT_ADDRESS, 1883)
+    mqtt_client.loop_forever()
+
+
+if __name__ == '__main__':
+    print('MQTT to InfluxDB bridge')
+    main()
