f927ffafdd892be96539187d67aedb92c28070a6
Jakob <jakob_luge@mailbox.org>
2022-01-16 18:02:54 +0100

    refactor java app

---
+++ MQTT_Publisher/src/MqttMain.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.util.Scanner;
+
+public class MqttMain {
+
+    private final static String LOCALHOST_BROKER = "tcp://localhost:1883";
+    private static String continuePublish = "Yes";
+
+    public static void main(String[] args) {
+        MqttPublisher publisher = new MqttPublisher(LOCALHOST_BROKER, "client_1");
+        publisherLoop(publisher);
+    }
+
+    public static void publisherLoop(MqttPublisher pub) {
+        Scanner scanner = new Scanner(System.in);
+        pub.generateMqttClient();
+        pub.connectClientToBroker();
+
+        while (continuePublish.equalsIgnoreCase("Yes")) {
+            System.out.println("Specify: topic,message,qos");
+            String input = scanner.nextLine();
+            System.out.println(input);
+            String[] params = input.split(",");
+            pub.publishMessage(params[0], params[1], Integer.valueOf(params[2]));
+            Toolkit.getDefaultToolkit().beep();
+            System.out.println("Continue publishing? yes/no");
+            continuePublish = scanner.nextLine();
+
+
+        }
+
+        pub.disconnectBroker();
+
+    }
+}
--- MQTT_Publisher/src/MqttPublisher.java
+++ MQTT_Publisher/src/MqttPublisher.java
@@ -4,51 +4,88 @@ import org.eclipse.paho.client.mqttv3.MqttException;
 import org.eclipse.paho.client.mqttv3.MqttMessage;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
-import java.util.Scanner;
-
 public class MqttPublisher {
 
-    public static void main(String[] args) {
+    private String brokerIp;
+    private String clientId;
+    private final MemoryPersistence persistence;
+    private MqttClient client;
+    private MqttConnectOptions opts;
 
-        String topic = "test";
-        String content = "";
-        int qos = 2;
-        String broker = "tcp://localhost:1885";
-        String clientId = "mqtt_test";
-        String nextPublish = "yes";
-        MemoryPersistence persistence = new MemoryPersistence();
+    public MqttPublisher(String brokerIp, String clientId) {
+        this.brokerIp = brokerIp;
+        this.clientId = clientId;
+        this.persistence = new MemoryPersistence();
 
+    }
+
+    public void generateMqttClient() {
         try {
-            Scanner scanner = new Scanner(System.in);
-            MqttClient mqttClient = new MqttClient(broker, clientId, persistence);
-            MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
-            System.out.println("Connecting to broker: " + broker);
-            mqttClient.connect(connOpts);
-            System.out.println("Connected to broker");
 
-            while(nextPublish.equals("yes")) {
-                System.out.println("Specify MQTT Message: ");
-                content = scanner.next();
-                System.out.println("Publishing message:" + content);
-                MqttMessage message = new MqttMessage(content.getBytes());
-                message.setQos(qos);
-                mqttClient.publish(topic, message);
-                System.out.println("Message published");
-                System.out.println("Publish another Message? (yes/no)");
-                nextPublish = scanner.next();
-            }
 
-            mqttClient.disconnect();
-            mqttClient.close();
-            System.exit(0);
-        } catch (MqttException me) {
-            System.out.println("reason " + me.getReasonCode());
-            System.out.println("msg " + me.getMessage());
-            System.out.println("loc " + me.getLocalizedMessage());
-            System.out.println("cause " + me.getCause());
-            System.out.println("excep " + me);
-            me.printStackTrace();
+            this.client = new MqttClient(this.brokerIp, this.clientId, this.persistence);
+            this.opts = new MqttConnectOptions();
+            this.opts.setCleanSession(true);
+        }catch(Exception e) {
+            System.out.println("[-] Error: could not create client");
         }
     }
+
+    public void connectClientToBroker() {
+        System.out.println("[+] Connecting to broker: " + this.brokerIp);
+        try {
+            this.client.connect(this.opts);
+            System.out.println("[+] Connected to broker");
+        } catch (MqttException e) {
+            System.out.println("[-] reason " + e.getReasonCode());
+            System.out.println("[-] msg " + e.getMessage());
+            System.out.println("[-] loc " + e.getLocalizedMessage());
+            System.out.println("[-] cause " + e.getCause());
+            System.out.println("[-] excep " + e);
+            e.printStackTrace();
+        }
+    }
+
+    public void publishMessage(String topic, String message, int qos) {
+        System.out.println("[+] Publishing message:" + message);
+
+        try {
+            MqttMessage mqttMessage = new MqttMessage(message.getBytes());
+            mqttMessage.setQos(qos);
+            this.client.publish(topic, mqttMessage);
+            System.out.println("[+] Message published");
+        } catch (Exception e) {
+            System.out.println("[-] Error: Publish failed");
+        }
+    }
+
+    public void disconnectBroker() {
+        try {
+            this.client.disconnect();
+            this.client.close();
+            System.exit(0);
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    public String getBrokerIp() {
+        return brokerIp;
+    }
+
+    public void setBrokerIp(String brokerIp) {
+        this.brokerIp = brokerIp;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+
 }
