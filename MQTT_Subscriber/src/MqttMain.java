d0ddb91f5078f75fa0a819dcbfe71f2b815375c8
Jakob <jakob_luge@mailbox.org>
2022-01-17 10:28:02 +0100

    add broker selection

--- MQTT_Subscriber/src/MqttMain.java
+++ MQTT_Subscriber/src/MqttMain.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
 public class MqttMain {
 
     private final static String LOCALHOST_BROKER = "tcp://localhost:1883";
 
 
     public static void main(String[] args) {
-        MqttSubscriber subscriber = new MqttSubscriber(LOCALHOST_BROKER,"client_2","test");
+        String broker = new String(LOCALHOST_BROKER);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Use localhost as broker?");
+        if(scanner.next().equalsIgnoreCase("no")){
+            System.out.println("Specify brokerIp:port");
+            broker = scanner.next();
+        }
+        MqttSubscriber subscriber = new MqttSubscriber(broker,"client_2","test");
         subscriber.generateMqttClient();
         subscriber.connectClientToBroker();
         subscriber.subscribe();
